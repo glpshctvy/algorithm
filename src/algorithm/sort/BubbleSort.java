d6cd834f9837421bf060232917f998d0f2b99a5e
glpshctvy <glpshctvy.okb@gmail.com>
2019-09-30 20:06:29 +0900

    last

--- src/algorithm/sort/BubbleSort.java
+++ src/algorithm/sort/BubbleSort.java
@@ -2,18 +2,16 @@ package algorithm.sort;
 
 public class BubbleSort implements Sortable {
 
-	@Override
-	public void sort(int[] messyArray) {
-		for (int i = 0; i < messyArray.length; i++) {
-
-			for (int j = messyArray.length; j > i + 1; j--) {
-				if(messyArray[j - 2]  >  messyArray[j - 1]) {
-					int temp = messyArray[j - 2];
-					messyArray[j - 2] = messyArray[j - 1];
-					messyArray[j - 1] = temp;
-				}
-			}
-		}
-	}
-
+  @Override
+  public void sort(int[] messyArray) {
+    for (int i = 0; i < messyArray.length; i++) {
+      for (int j = messyArray.length; j > i + 1; j--) {
+        if (messyArray[j - 2] > messyArray[j - 1]) {
+          int temp = messyArray[j - 2];
+          messyArray[j - 2] = messyArray[j - 1];
+          messyArray[j - 1] = temp;
+        }
+      }
+    }
+  }
 }
